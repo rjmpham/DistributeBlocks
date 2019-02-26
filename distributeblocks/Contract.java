735da7f388c607c3298005b7f986abcca1dde2f7
Ian <ieliopou@ucalgary.ca>
2019-02-26 15:14:47 -0700

    added generate,get, and verify signature methods to contract, added a timestamp field to contract, calculateHash() is now based on timestamp instead of number of contracts in order to replicate hash, contract constructor now includes privatekey and signs contract upon creation, signatures are based on transaction hash not just the public keys like in tutorial

--- distributeblocks/Contract.java
+++ distributeblocks/Contract.java
@@ -6,14 +6,19 @@
  * some amount of coin in one direction, but may be used
  * several times.
  *
- * Signatures have yet to be implemented and would verify
- * that it is the sender authorizing the sending of coin.
+ * Signatures verify that it is the sender authorizing the sending of coin.
+ * Signatures sign hash/id_contract of transaction (see calculateHash())
+ * 
+ * Contracts are signed upon creation by the sender
+ * (this is different from the tutorial which required 2 separate steps (create then sign))
  */
 package distributeblocks;
 
 
 import java.security.*;
 import java.util.ArrayList;
+import java.util.Date;
+
 import distributeblocks.crypto.*;
 
 public class Contract {
@@ -24,40 +29,71 @@ public class Contract {
   public byte[] signature; // for user's personal wallet
   public ArrayList<Contract_In> input = new ArrayList<Contract_In>();
   public ArrayList<Contract_Out> output = new ArrayList<Contract_Out>();
-  private static int count_Contracts = 0; // estimates number of contracts created.
+  //private static int count_Contracts = 0; // estimates number of contracts created.
+  private long timestamp; //timestamp for the block
 
   /*
    * Generating contracts requires the public keys of both
    * the sender and reciever as well as the ammount.
    */
-  public Contract(PublicKey send,PublicKey recieve , float amount,  ArrayList<Contract_In> varriables) {
+  public Contract(PrivateKey senderPrivateKey, PublicKey send,PublicKey recieve , float amount,  ArrayList<Contract_In> varriables) {
 		this.pk_Sender = send;
 		this.pk_Receiver = recieve;
 		this.exchange = amount;
 		this.input = varriables;
+		this.timestamp = new Date().getTime();
+		try {
+			this.id_Contract = calculateHash();
+		} catch (FailedToHashException e) {
+			e.printStackTrace();
+		}
+		generateSignature(senderPrivateKey);
 	}
 
   // Calculate id_Contract
-  private String calulateHash() throws FailedToHashException{
-    count_Contracts++; //method to prevent identical hashes
+  //This hash is based on the public keys of the sender and receiver,
+  //the amount to be sent, and the timestamp of the transaction
+  private String calculateHash() throws FailedToHashException{
+    //count_Contracts++; //method to prevent identical hashes
     return Crypto.calculateObjectHash(
       Crypto.keyToString(pk_Sender) +
       Crypto.keyToString(pk_Receiver) +
-      Float.toString(exchange) + count_Contracts
+      Float.toString(exchange) + timestamp
       );
   }
 
-
-  // Method to handle the contract. Returns true of the contract is created
-  public boolean contractEnforcer() throws FailedToHashException{
-
-    //We haven't implemented signatures yet
   /*
-  		if(verifiySignature() == false) {
+   * Input: The private key used to sign a contract
+   * Details: Signs the hash/id of the contract
+   * (which is a hash of the public keys for the sender/receiver, the amount sent, and the number of contracts in existence)
+   * Output: Sets the signature field of this contract class
+   */
+  public void generateSignature( PrivateKey privateKey ) {
+	  this.signature = Crypto.signMessage(privateKey, this.id_Contract );
+	  return;
+  }
+  
+  //A method to return this contract's signature
+  public byte[] getSignature() {
+	  return this.signature;
+  }
+  
+  /*
+   * Details: Verifies that the signature of this contract is correct by seeing if the
+   * signature and hash/id of this contract correspond to the public key of the sender
+   * Output: Returns true if the signature matches the public key of the sender
+   */
+  public boolean verifySignature() {
+	  return Crypto.verifySignature(this.pk_Sender, this.id_Contract, this.signature);
+  }
+  
+  // Method to handle the contract. Returns true if the contract is created
+  public boolean contractEnforcer() throws FailedToHashException{
+  		if(verifySignature() == false) {
   			System.out.println("#Transaction Signature failed to verify");
   			return false;
   		}
-  */
+  
   		//gather contracts that are inputs (Make sure they are unspent):
   		for(Contract_In i : input) {
   			i.funds = testDriver.funds_HashMap.get(i.id_Contract_Out);
@@ -72,7 +108,7 @@ public class Contract {
   		//generate contract output:
   		float remaining = getExchangeAmount() - exchange;
       //get exchange amount of input then the left over change:
-  		id_Contract = calulateHash();
+  		//id_Contract = calculateHash(); this is no longer necessary but leaving it in case want to revert, should delete once confidence is gained
   		output.add(new Contract_Out( this.pk_Receiver, exchange,id_Contract)); //send exchange to reciever
   		output.add(new Contract_Out( this.pk_Sender, remaining,id_Contract)); //send the left over 'change' back to sender
 
