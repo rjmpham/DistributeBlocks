e87df2f161d6967af82fe570f8e505a4f976714d
Eric Dimond <eric.dimond@ucalgary.ca>
2019-03-30 19:46:11 -0600

    removed unneeded import

--- distributeblocks/Wallet.java
+++ distributeblocks/Wallet.java
@@ -2,7 +2,6 @@ package distributeblocks;
 
 import java.security.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import distributeblocks.crypto.*;
