e04575a859d6ab32b5ad5bd8d286644bc5adf1d1
Eric Dimond <eric.dimond@ucalgary.ca>
2019-04-04 11:50:37 -0600

    a console somewhat works

--- distributeblocks/io/Console.java
+++ distributeblocks/io/Console.java
@@ -1,63 +1,106 @@
 package distributeblocks.io;
 
-/*
- * The classes in this file are based on those from:
- * 		http://www.java2s.com/Tutorials/Java/Swing_How_to/JFrame/Create_Console_JFrame.htm
- * Credit for the original code goes to the author of that post.
- */
-import java.awt.Color;
-import java.awt.Font;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.PrintStream;
 
-import javax.swing.JFrame;
-import javax.swing.JTextArea;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.Group; 
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.stage.Stage; 
 
-public class Console extends JFrame {
+public class Console extends Application {
 	
-	public static void main(String[] args) {
-		ConsoleWindow consoleWindow = new ConsoleWindow();
-		consoleWindow.init();
-		Console launcher = new Console();
-		launcher.setVisible(true);
-		consoleWindow.getFrame().setLocation(
-				launcher.getX() + launcher.getWidth() + launcher.getInsets().right,
-				launcher.getY());
-	}
+	private int width = 600;
+	private int height = 300;
+	
+	@Override 
+	public void start(Stage stage) {  
+		TextArea textArea = new TextArea();
+		textArea.setPrefHeight(height);  
+		textArea.setPrefWidth(width);
+		
+		ConsoleController consoleController = new ConsoleController(textArea);
+		
+		PrintStream printer = new PrintStream(consoleController, true);
+		
+		System.setOut(printer);
+		System.setErr(printer);
+		
+		//Creating a Group object  
+		Group root = new Group(consoleController.getTextArea());   
+           
+		//Creating a scene object 
+		Scene scene = new Scene(root, width, height);  
+  
+		//Setting title to the Stage 
+		stage.setTitle("Network Log"); 
+		
+		//Adding scene to the stage 
+		stage.setScene(scene); 
+     
+		//Displaying the contents of the stage 
+		stage.show(); 
+	}      
+	   
+	public static void main(String args[]){ 
+	      
+		Thread object = new Thread(new Runnable() {
 
-	private Console() {
-		super();
-		setSize(600, 600);
-		setResizable(false);
-		setDefaultCloseOperation(EXIT_ON_CLOSE);
-	}
-}
-
-class ConsoleWindow {
-	final JFrame frame = new JFrame();
-	public ConsoleWindow() {
-		JTextArea textArea = new JTextArea(24, 80);
-		textArea.setBackground(Color.BLACK);
-		textArea.setForeground(Color.LIGHT_GRAY);
-		textArea.setFont(new Font(Font.MONOSPACED, Font.PLAIN, 12));
-    
-		System.setOut(new PrintStream(new OutputStream() {
-      
-			@Override
-			public void write(int b) throws IOException {
-				textArea.append(String.valueOf((char) b));
+		@Override
+		public void run() {
+			while(true) {
+				System.out.println("it works!"); 
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			}
-		}));
-		frame.add(textArea);
+			
+		}});
+        object.start(); 
+          
+        launch(args); 
 	}
 	
-	public void init() {
-		frame.pack();
-		frame.setVisible(true);
-	}
 	
-	public JFrame getFrame() {
-		return frame;
+	public class ConsoleController extends OutputStream {
+
+		@FXML
+		public TextArea textArea = new TextArea();	// default size
+		
+		public ConsoleController() {}
+		
+		public ConsoleController(TextArea textArea) {
+			this.textArea = textArea;
+		}
+		    	
+        @Override
+        public void write(int b) throws IOException {
+            appendText(String.valueOf((char) b));
+        }
+
+        @Override
+        public void write(byte[] b, int off, int len) throws IOException {
+            appendText(new String(b, off, len));
+        }
+
+        @Override
+        public void write(byte[] b) throws IOException {
+            write(b, 0, b.length);
+        }
+       
+        public void appendText(String str) {
+	        Platform.runLater(() -> textArea.appendText(str));
+	    }
+        
+        public TextArea getTextArea() {
+        	return textArea;
+        }
 	}
 }
