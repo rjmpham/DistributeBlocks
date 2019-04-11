16654daf4611dcc73823de6a9556d967b09ff0d7
rjmpham <rpham@localhost.localdomain>
2019-04-11 06:30:29 -0600

    Attempt at DDoS verson of the TransactionBroadcastMessageProcessor

--- distributeblocks/net/processor/TransactionBroadcastMessageProcessor.java
+++ distributeblocks/net/processor/TransactionBroadcastMessageProcessor.java
@@ -1,22 +1,41 @@
 package distributeblocks.net.processor;
+import distributeblocks.io.DirectoryManager;
+import distributeblocks.io.WalletManager;
 import distributeblocks.util.Validator;
 import distributeblocks.BlockChain;
 import distributeblocks.net.NetworkService;
 import distributeblocks.net.message.TransactionBroadcastMessage;
 import distributeblocks.io.Console;
+import distributeblocks.crypto.*;
+
+import java.io.IOException;
+import java.security.PublicKey;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 
 public class TransactionBroadcastMessageProcessor extends AbstractMessageProcessor<TransactionBroadcastMessage> {
 	@Override
 	public void processMessage(TransactionBroadcastMessage message) {
 
 		Console.log("Got transaction broadcast message.");
-		
-		if(message.transaction.verifySignature()) {
-			NetworkService.getNetworkManager().addTransaction(message.transaction);
-		}
-		else {
-			Console.log("Transaction signature failed to verify");
+		String fullPath = DirectoryManager.fullPathToDir("jalal");
+		try {
+			PublicKey publicKey = WalletManager.loadPublicKey(fullPath, Crypto.GEN_ALGORITHM);
+			if(message.transaction.verifySignature()) {
+				if (Crypto.verifySignature(publicKey, message.transaction.getTransactionId(),message.transaction.getSignature()){
+					return
+				}else{
+					NetworkService.getNetworkManager().addTransaction(message.transaction);
+					return;
+				}
+			}
+		}catch (NoSuchAlgorithmException | InvalidKeySpecException | IOException e) {
+			Console.log("DDoS TransactionBroadcastMessageProcessor error");
 		}
 
-	}
+			Console.log("Transaction signature failed to verify");
+		return;
+		}
+
+
 }
